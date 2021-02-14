942e5c1b042735d5e3e68bf6391189fdc3d80993
Muhammad Rifat <hrifat450@gmail.com>
2021-02-14 11:58:35 +0600

    added not found message

--- app.js
+++ app.js
@@ -30,10 +30,21 @@ const showImages = (images) => {
 }
 
 const getImages = (query) => {
-  displaySpinner();
   fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
     .then(response => response.json())
-    .then(data => showImages(data.hits))
+    .then(data => {
+      if(data.hits.length === 0){
+        document.getElementById('message-notFound').style.display = "block";
+        document.getElementById('message-notFound').innerText = "No Images Found";
+        document.getElementById('result-images').style.display = "none";
+      }
+      else{
+        document.getElementById('message-notFound').style.display = "none";
+        document.getElementById('result-images').style.display = "block";
+        displaySpinner();
+        showImages(data.hits);
+      }
+    })
     .catch(err => console.log(err))
 }
 
